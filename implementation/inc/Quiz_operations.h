8d32d48d318ecc989d1231ba2b714666468024ae
Maha Lakshmi Panguluri <55955612+3114-HM@users.noreply.github.com>
2021-09-07 09:43:11 +0530

    Update Quiz_operations.h

--- implementation/inc/Quiz_operations.h
+++ implementation/inc/Quiz_operations.h
@@ -1,1 +1,25 @@
+#ifndef __QUIZ_OPERATIONS_H_INCLUDED
+#define __QUIZ_OPERATIONS_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+int main(
+  char quest [11][200],
+  char optionl [3][20],
+  char option2 [3][20], 
+  char option3 [3][20],
+  char option4 [3][20], 
+  char option5 [3][20],
+  char option6 [3][20],
+  char option7 [3][20],
+  char option8 [3][20],
+  char option9 [3][20],
+  char option10 [3][20],
+  char option11 [3][20],
+  int response[11], 
+  int correct_ans[11], 
+  int option,
+  int i, 
+  int marks);
+  #endif 
