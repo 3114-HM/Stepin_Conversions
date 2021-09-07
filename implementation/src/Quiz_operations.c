3d0c56468e230e11504097ddc860b399e1e82220
Maha Lakshmi Panguluri <55955612+3114-HM@users.noreply.github.com>
2021-09-07 09:47:19 +0530

    Update Quiz_operations.c

--- implementation/src/Quiz_operations.c
+++ implementation/src/Quiz_operations.c
@@ -1,1 +1,24 @@
-
+#include<Quiz_operations.h>
+int main( 
+  char quest [11][200],
+  char optionl [3][20],
+  char option2 [3][20], 
+  char option3 [3][20],
+  char option4 [3][20], 
+  char option5 [3][20],
+  char option6 [3][20],
+  char option7 [3][20],
+  char option8 [3][20],
+  char option9 [3][20],
+  char option10 [3][20],
+  char option11 [3][20],
+  int response[11],int correct_ans[11], int option,int i, int marks){    
+           marks = 0;  
+           for(i = 0;i <= 10;i++)  {   
+             if(correct_ans[i]+1==response[i])   {   
+                marks++;   
+             }  
+           }        
+      while(option!=4);
+      return 0;
+  }
